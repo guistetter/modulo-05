9d970ea6c63666e6b5e2dcc09da191ff106babe2
guistetter <guilherme_stetter@hotmail.com>
2020-10-06 15:05:47 -0300

    controlando estado

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+  };
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositorio" />
+  handleInputChange = (e) => {
+    this.setState({ newRepo: e.target.value });
+  };
 
-        <SubmitButton disable>
-          <FaPlus color="#333" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.newRepo);
+  };
+
+  render() {
+    const { newRepo } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositorio"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton disable>
+            <FaPlus color="#333" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
-
-export default Main;
