1ce3c2341bcda7f391a59618967e9113274de7e2
guistetter <guilherme_stetter@hotmail.com>
2020-10-06 15:37:25 -0300

    animando botao ao loading

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -32,6 +32,13 @@ export const Form = styled.form`
     font-size: 16px;
   }
 `;
+const rotate = keyframes`
+  from{
+    transform: rotate:(0deg);
+  }
+  to{
+  }transform: rotate:(360deg);
+`;
 
 export const SubmitButton = styled.button.attrs((props) => ({
   type: 'submit',
@@ -51,4 +58,12 @@ export const SubmitButton = styled.button.attrs((props) => ({
     cursor: not-allowed;
     opacity: 0.6;
   }
+
+  ${(props) =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
 `;
